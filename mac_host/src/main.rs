8cdd8295e625bed8577882ab062899e231305ee1
Stuart Harris <stuart.harris@red-badger.com>
2020-11-27 14:18:41 +0000

    refactor mac host

--- mac_host/src/main.rs
+++ mac_host/src/main.rs
@@ -1,29 +1,31 @@
 use std::collections::HashMap;
 use wascc_host::{Actor, Host, NativeCapability};
+use wascc_httpsrv::HttpServerProvider;
+
+const CAP_OLED_PROVIDER: &str = "red-badger:oled-ssd1306";
 
 fn main() -> std::result::Result<(), Box<dyn std::error::Error>> {
     let _ = env_logger::try_init();
+
     let host = Host::new();
-    host.add_actor(Actor::from_file(
+
+    let http_server_provider = NativeCapability::from_instance(HttpServerProvider::new(), None)?;
+    let cap_http_server_provider = &http_server_provider.descriptor().id.clone();
+    host.add_native_capability(http_server_provider)?;
+
+    let actor = Actor::from_file(
         "../wasm_oled_actor/target/wasm32-unknown-unknown/debug/wasm_oled_actor_signed.wasm",
-    )?)?;
-    host.add_native_capability(NativeCapability::from_file(
-        "../../../src/capability-providers/http-server/target/debug/libwascc_httpsrv.dylib",
-        None,
-    )?)?;
+    )?;
+    let actor_pub_key = &actor.public_key();
+    host.add_actor(actor)?;
 
     host.set_binding(
-        "MDNKUUYGVK2OSEH5EUBRF2UYABTTYZFZCF6WA5OF7GURJFB235CLUZMN",
-        "wascc:http_server",
+        actor_pub_key,
+        cap_http_server_provider,
         None,
         generate_port_config(8081),
     )?;
-    host.set_binding(
-        "MDNKUUYGVK2OSEH5EUBRF2UYABTTYZFZCF6WA5OF7GURJFB235CLUZMN",
-        "red-badger:oled-ssd1306",
-        None,
-        HashMap::new(),
-    )?;
+    host.set_binding(actor_pub_key, CAP_OLED_PROVIDER, None, HashMap::new())?;
 
     std::thread::park();
 
