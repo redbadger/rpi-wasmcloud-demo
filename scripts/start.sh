78b94fd4d06594ffc2adc8316429de012b705c6b
Stuart Harris <stuart.harris@red-badger.com>
2021-04-22 15:22:49 +0100

    make start script idempotent

--- scripts/start.sh
+++ scripts/start.sh
@@ -8,11 +8,11 @@ PROVIDER_HTTP=VAG3QITQQ2ODAOWB5TTQSDJ53XK3SHBEIFNK4AYJ5RKAX2UNSCAPHA5M
 PROVIDER_LOGGING=VCCANMDC7KONJK435W6T7JFEEL7S3ZG6GUZMZ3FHTBZ32OZHJQR5MJWZ
 PROVIDER_OLED=VCLB2N33XBBAVPVHIARI7JJJSO4SC2GVAWQ43EKHTPHKKFUCI5OLZQ2Q
 
-wash ctl start provider redbadger.azurecr.io/oled_ssd1306_provider:0.0.1 --constraint "name=pi-01"
-wash ctl start provider wasmcloud.azurecr.io/httpserver:0.11.1 --constraint "hostcore.os=macos"
-wash ctl start provider wasmcloud.azurecr.io/logging:0.9.1 --constraint "hostcore.os=macos"
+wash ctl start provider redbadger.azurecr.io/oled_ssd1306_provider:0.0.1 --constraint "name=pi-01" || true
+wash ctl start provider wasmcloud.azurecr.io/httpserver:0.11.1 --constraint "hostcore.os=macos" || true
+wash ctl start provider wasmcloud.azurecr.io/logging:0.9.1 --constraint "hostcore.os=macos" || true
 
-wash ctl start actor redbadger.azurecr.io/oled_actor:0.0.1 --constraint "name=pi-02"
+wash ctl start actor redbadger.azurecr.io/oled_actor:0.0.1 --constraint "name=pi-02" || true
 
 wash ctl link $ACTOR $PROVIDER_EXTRAS wasmcloud:extras
 wash ctl link $ACTOR $PROVIDER_HTTP wasmcloud:httpserver PORT=8081
