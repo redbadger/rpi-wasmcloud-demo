1accd2f84e2ac137dd640aec3705ac3e838b7ece
Stuart Harris <stuart.harris@red-badger.com>
2021-10-14 09:18:31 +0100

    all build now

--- provider/src/main.rs
+++ provider/src/main.rs
@@ -1,16 +1,9 @@
-use anyhow::Result;
+use display::say;
+use log::debug;
 use oled_ssd1306_interface::{Oled, OledReceiver, Request};
 use wasmbus_rpc::provider::prelude::*;
 
-#[cfg(target_os = "aarch64")]
 mod display;
-#[cfg(target_os = "aarch64")]
-use anyhow::anyhow;
-#[cfg(target_os = "aarch64")]
-use display::say;
-#[cfg(target_os = "aarch64")]
-use log::debug;
-
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     provider_main(OledProvider::default())?;
 
@@ -30,30 +23,17 @@ impl ProviderHandler for OledProvider {}
 #[async_trait]
 impl Oled for OledProvider {
     /// updates the text on the oled display
-    #[cfg(target_os = "aarch64")]
     async fn update(&self, _ctx: &Context, req: &Request) -> RpcResult<()> {
         debug!("processing request update({})", req.text);
-        say(&msg.txt).map_err(|e| anyhow!("error writing to display: {:?}", e))?;
+        say(&req.text)
+            .map_err(|e| RpcError::Other(format!("error writing to display: {:?}", e)))?;
         Ok(())
     }
 
     /// clears the oled display
-    #[cfg(target_os = "aarch64")]
     async fn clear(&self, _ctx: &Context) -> RpcResult<()> {
         debug!("processing request clear()");
-        say("").map_err(|e| anyhow!("error writing to display: {:?}", e))?;
+        say("").map_err(|e| RpcError::Other(format!("error writing to display: {:?}", e)))?;
         Ok(())
-    }
-
-    /// not implemented!
-    #[cfg(not(target_os = "aarch64"))]
-    async fn update(&self, _ctx: &Context, _req: &Request) -> RpcResult<()> {
-        unimplemented!()
-    }
-
-    /// not implemented!
-    #[cfg(not(target_os = "aarch64"))]
-    async fn clear(&self, _ctx: &Context) -> RpcResult<()> {
-        unimplemented!()
     }
 }
