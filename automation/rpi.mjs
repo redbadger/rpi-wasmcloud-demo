fba17949f2b55fa7fce628b4ec7566bf4559844f
Stuart Harris <stuart.harris@red-badger.com>
2022-01-06 17:13:11 +0000

    fix env

--- automation/rpi.mjs
+++ automation/rpi.mjs
@@ -2,21 +2,18 @@
 
 import { step } from "./lib.mjs";
 
-const ENV = {
-  WASMCLOUD_CLUSTER_SEED:
-    "SCANP3E75PCKS5AF2UI56HBJ5HVGYVXL52ZJS35S6MVHOYB7LAAXSU6B24",
-  WASMCLOUD_CLUSTER_ISSUERS:
-    "CDAM4OLLU5ZKQTWXYCGJ2IKMAFIHFCXTBIEOAGUDK26KUVJAH3RCXGUS",
-  WASMCLOUD_CTL_HOST: "stuarts-macbook-pro.local",
-  WASMCLOUD_RPC_HOST: "stuarts-macbook-pro.local",
-  WASMCLOUD_PROV_RPC_HOST: "stuarts-macbook-pro.local",
-  WASMCLOUD_OCI_ALLOWED_INSECURE: "registry:5001",
-  HOST_NODE: os.hostname(),
-};
+process.env.WASMCLOUD_CLUSTER_SEED =
+  "SCANP3E75PCKS5AF2UI56HBJ5HVGYVXL52ZJS35S6MVHOYB7LAAXSU6B24";
+process.env.WASMCLOUD_CLUSTER_ISSUERS =
+  "CDAM4OLLU5ZKQTWXYCGJ2IKMAFIHFCXTBIEOAGUDK26KUVJAH3RCXGUS";
+process.env.WASMCLOUD_CTL_HOST = "stuarts-macbook-pro.local";
+process.env.WASMCLOUD_RPC_HOST = "stuarts-macbook-pro.local";
+process.env.WASMCLOUD_PROV_RPC_HOST = "stuarts-macbook-pro.local";
+process.env.WASMCLOUD_OCI_ALLOWED_INSECURE = "registry:5001";
+process.env.HOST_NODE = os.hostname();
 
 if (argv.up) {
   step("Starting wasmcloud");
   cd(`${os.homedir()}/wasmcloud-otp/host_core`); // TODO: make this better :-)
-  await $`
-  	${[...Object.keys(ENV).map((k) => `${k}=${ENV[k]}`)]} make run-interactive`;
+  await $`make run-interactive`;
 }
