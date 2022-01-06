682c57606648b8cb055e99b8ba5e5090eb72bd72
Stuart Harris <stuart.harris@red-badger.com>
2022-01-06 17:49:04 +0000

    debugging capability

--- automation/macos.mjs
+++ automation/macos.mjs
@@ -9,7 +9,7 @@ const WASMCLOUD_CLUSTER_ISSUERS =
   "CDAM4OLLU5ZKQTWXYCGJ2IKMAFIHFCXTBIEOAGUDK26KUVJAH3RCXGUS";
 const ACTOR = {
   id: "MC5QO34YH43RO6R3AMM3I4XC7ET2KXEMXLW4CX3XFQR4XWGF6QREPPBH",
-  ref: `${REGISTRY}/oled_actor:0.0.1`,
+  ref: `${REGISTRY}/oled_actor:0.1.1`,
 };
 const HTTPSERVER = {
   id: "VAG3QITQQ2ODAOWB5TTQSDJ53XK3SHBEIFNK4AYJ5RKAX2UNSCAPHA5M",
@@ -19,7 +19,7 @@ const HTTPSERVER = {
 };
 const OLED = {
   id: "VCLB2N33XBBAVPVHIARI7JJJSO4SC2GVAWQ43EKHTPHKKFUCI5OLZQ2Q",
-  ref: `${REGISTRY}/oled_ssd1306_provider:0.0.1`,
+  ref: `${REGISTRY}/oled-ssd1306-provider:0.1.0`,
   contract: "red-badger:oled-ssd1306",
 };
 
@@ -35,31 +35,13 @@ if (argv.start) {
   step("starting workloads");
   //   await $`(cd ../actor && make push)`;
   //   await $`(cd ../provider && make push)`;
-  //   await $`wash ctl start actor ${ACTOR.ref} --timeout 30`;
+  await $`wash ctl start actor ${ACTOR.ref} --timeout 30 --constraint node=pi-01`;
 
   await $`wash ctl link put ${ACTOR.id} ${HTTPSERVER.id} ${HTTPSERVER.contract} ${HTTPSERVER.config}`;
-  //   await $`wash ctl start provider ${HTTPSERVER.ref} --link-name default --timeout 30`;
+  await $`wash ctl start provider ${HTTPSERVER.ref} --link-name default --timeout 30 --constraint node=MacOS`;
 
   await $`wash ctl link put ${ACTOR.id} ${OLED.id} ${OLED.contract}`;
-  //   await $`wash ctl start provider ${OLED.ref} --link-name default --timeout 30`;
-}
-
-if (argv.restart_actor) {
-  step("restarting actor");
-  const host = await getHost();
-  await $`wash ctl stop actor ${host} ${ACTOR.id} --timeout 30`;
-  await $`wash drain all`;
-  await $`(cd ../actor && make push)`;
-  await $`wash ctl start actor ${ACTOR.ref} --timeout 30`;
-}
-
-if (argv.restart_provider) {
-  step("restarting provider");
-  const host = await getHost();
-  await $`wash ctl stop provider ${host} ${OLED.id} default ${OLED.contract} --timeout 30`;
-  await $`wash drain all`;
-  await $`(cd ../provider && make push)`;
-  await $`wash ctl start provider ${OLED.ref} --link-name default --timeout 30`;
+  await $`wash ctl start provider ${OLED.ref} --link-name default --timeout 30 --constraint node=pi-02`;
 }
 
 if (argv.stop) {
